13c704957a8e81b30e73c0607622c9e2fa1e8838
Bogdan Khodchenko <khoch3nk0@gmail.com>
2021-03-10 19:55:25 +0100

    deleted junk

--- app/src/main/java/com/example/doyouknowkoko/ReadActivity.kt
+++ app/src/main/java/com/example/doyouknowkoko/ReadActivity.kt
@@ -6,6 +6,7 @@ import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.database.*
 import java.util.*
+
 //TODO DELETE
 class ReadActivity : AppCompatActivity() {
     private var listViewReadData: ListView? = null
@@ -15,29 +16,29 @@ class ReadActivity : AppCompatActivity() {
     private val USER_KEY = "Outfit"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.read_layout)
-        listViewReadData = findViewById(R.id.listView_ReadData)
-        listData = ArrayList()
-        arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, listData as ArrayList<String>)
-        listViewReadData?.setAdapter(arrayAdapter)
-        dataBase = FirebaseDatabase.getInstance().getReference(USER_KEY)
-        dataFromDataBase
+        //setContentView(R.layout.read_layout)
+        //listViewReadData = findViewById(R.id.listView_ReadData)
+//        listData = ArrayList()
+//        arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, listData as ArrayList<String>)
+//        listViewReadData?.setAdapter(arrayAdapter)
+//        dataBase = FirebaseDatabase.getInstance().getReference(USER_KEY)
+//        dataFromDataBase
     }
 
-    private val dataFromDataBase: Unit
-        private get() {
-            val valueEventListener: ValueEventListener = object : ValueEventListener {
-                override fun onDataChange(snapshot: DataSnapshot) {
-                    if (listData!!.size > 0) listData?.clear()
-                    for (dataSnapshot in snapshot.children) {
-                        val outfit = dataSnapshot.getValue(Outfit::class.java)!!
-                        listData!!.add(outfit.name)
-                    }
-                    arrayAdapter!!.notifyDataSetChanged()
-                }
-
-                override fun onCancelled(error: DatabaseError) {}
-            }
-            dataBase!!.addValueEventListener(valueEventListener)
-        }
+//    private val dataFromDataBase: Unit
+//        private get() {
+//            val valueEventListener: ValueEventListener = object : ValueEventListener {
+//                override fun onDataChange(snapshot: DataSnapshot) {
+//                    if (listData!!.size > 0) listData?.clear()
+//                    for (dataSnapshot in snapshot.children) {
+//                        val outfit = dataSnapshot.getValue(Outfit::class.java)!!
+//                        listData!!.add(outfit.name)
+//                    }
+//                    arrayAdapter!!.notifyDataSetChanged()
+//                }
+//
+//                override fun onCancelled(error: DatabaseError) {}
+//            }
+//            dataBase!!.addValueEventListener(valueEventListener)
+//        }
 }
